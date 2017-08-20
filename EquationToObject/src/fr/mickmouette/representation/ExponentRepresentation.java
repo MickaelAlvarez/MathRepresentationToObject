6cd3b781a37810e0f91c5cb998825522ca711620
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2017-08-20 12:12:21 +0200

    Add licence on each files

--- EquationToObject/src/fr/mickmouette/representation/ExponentRepresentation.java
+++ EquationToObject/src/fr/mickmouette/representation/ExponentRepresentation.java
@@ -5,6 +5,29 @@ import fr.mickmouette.core.elements.generated.BinaryOperator;
 import fr.mickmouette.core.elements.representation.BinaryOperatorRepresentation;
 import fr.mickmouette.generated.Exponent;
 
+/**
+ *  This project is a library developed for java object developers. It provide a way to easily manipulate infix mathematical equations.
+ *  Copyright (C) 2017  Mickael Alvarez
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Contact : mickael.alvarez.93@gmail.com
+ * 
+ * @author Mickael Alvarez
+ *
+ * @param <V>
+ */
 public abstract class ExponentRepresentation<V> extends BinaryOperatorRepresentation<V> {
 	protected abstract Exponent<V> buildExponent(IEquation<V> leftEq, IEquation<V> rightEq);
 	
